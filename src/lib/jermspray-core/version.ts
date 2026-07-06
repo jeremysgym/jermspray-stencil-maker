ae46fdb992961b32695a68c35494990914581c77
jeremysgym <jeremystaylor67@gmail.com>
2026-07-06 16:52:35 -0500

    Create JermSpray Core project structure

---
+++ src/lib/jermspray-core/version.ts
@@ -0,0 +1,8 @@
+export const CORE_NAME = "JermSpray Core";
+
+export const CORE_VERSION = "2.0.0-alpha.1";
+
+export const CORE_AUTHOR = "Jeremy Taylor";
+
+export const CORE_DESCRIPTION =
+  "Professional SVG stencil generation engine.";
